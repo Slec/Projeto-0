b82e11afac82e923b04a78f65109e9633e8126ca
Igor <igorscv@hotmail.com>
2023-07-01 02:24:20 -0300

    moved files to another folder + setup them correctly

--- script.js
+++ script.js
@@ -8,11 +8,11 @@ function toggleMode() {
   // substituir imagem
   if (html.classList.contains("light")) {
     // se tiver light mode, adicionar a imagem light
-    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute("src", "./.vscode/assets/avatar-light.png")
     img.setAttribute("alt", "Foto de Igor Coelho andando de slackline na praia")
   } else {
     // se tiver sem light mode, adicionar imagem padrão
-    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute("src", "./.vscode/assets/avatar.png")
     img.setAttribute("alt", "Foto de Igor Coelho sorrindo.")
   }
 }
